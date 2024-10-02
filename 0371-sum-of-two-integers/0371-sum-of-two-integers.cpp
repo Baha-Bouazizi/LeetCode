7723aebfe493512c1d94284f0e6f2461273faa49
Bouazizi Baha <166825233+Baha-Bouazizi@users.noreply.github.com>
2024-10-02 15:25:03 +0100

    Time: 0 ms (100.00%), Space: 7.3 MB (74.57%) - LeetHub

--- 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
+++ 0371-sum-of-two-integers/0371-sum-of-two-integers.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-  return a+b ;
+        
+        if(a>=0 && b>=0){
+            while(b--){
+                a++;
+            }
+            return a;
+        }else if(b<0 && a<0){
+            while(a<0){
+                a++;
+                b--;
+            }
+            return b;
+        }else{
+            if(a<0 && b>=0){
+                int aux=a;
+                a=b;
+                b=aux;
+            }
+            while(a--){
+                b++;
+            }
+            return b;
+        }
 }
 };

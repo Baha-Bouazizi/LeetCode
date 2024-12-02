529538f82ea1ffee46bafdfa06314a706e9979ce
Bouazizi Baha <166825233+Baha-Bouazizi@users.noreply.github.com>
2024-12-02 22:49:01 +0100

    Time: 0 ms (100%), Space: 8.2 MB (9.77%) - LeetHub

---
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int isPrefixOfWord(string s, string sW) {
+        string a="";
+        int cnt=1;
+        for(int i=0; i<s.size(); i++){
+            if(isalpha(s[i])) a+=s[i];
+            else{
+                a="";
+                cnt++;
+            } 
+            if(a.substr(0,sW.size())==sW) return cnt;
+        }
+        return -1;
+    }
+};
